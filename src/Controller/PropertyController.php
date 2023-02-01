d8712cdbfdfd02bd340dd978f5cd06f499d25284
Paul78330 <paul.gaultier76@yahoo.com>
2023-02-01 15:55:42 +0100

    premieres_pages

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PropertyController extends AbstractController
+{
+
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+
+
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
+    /**
+     * @Route("/biens", name="property.index")
+     */
+    public function index(): Response
+    {
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties',
+        ]);
+    }
+
+
+    /**
+
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     * @param Property $property
+     * @param string $slug
+     * @return Response
+     */
+    public function show(Property $property, string $slug): Response
+    {
+
+        if($property->getSlug() !== $slug )
+        {
+            return $this->redirectToRoute('property.show',[
+                'id' => $property->getId(),
+                'slug'=> $property->getSlug()
+            ], 301); //satut 301 car rédirection permanante
+        }
+
+        return $this->render('property/show.html.twig', [
+            'property' => $property,
+            'current_menu' => 'properties'
+        ]);
+
+    }
+
+
+
+}
